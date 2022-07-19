938dae159ba0a0d7cf5f39119a1196e39575f8d2
gitgroch <geoff.rochford@gmail.com>
2022-07-19 13:44:31 +0000

    Add hangman art

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
+import random
 
-words = 'aluminium key giant location drawing ignorance cancer establish deficiency prosper learn print gravity joint architect charge exaggerate state dorm piano '
 
-def getRandomWord():
+words = 'aluminium key giant location drawing ignorance cancer establish prosper learn print gravity joint architect charge exaggerate state dorm piano'.split()
+
+def getRandomWord(wordList):
     '''
     Returns a random word (string) from a word list
     '''
     wordIndex = random.randint(0, len(wordList) -1)
+    # store a random index for list in variable 
     return wordList[wordIndex]
+    # return the elementin list at the random index stored 
+    print(wordList)
+
+
 
