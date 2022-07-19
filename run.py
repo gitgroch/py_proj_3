d84718d9f56471f27cc7372e755abb0485100980
gitgroch <geoff.rochford@gmail.com>
2022-07-19 14:23:04 +0000

    Add function to get player guess

--- run.py
+++ run.py
@@ -41,9 +41,9 @@ def get_random_word(word_list):
     Returns a random word (string) from a word list
     '''
     word_index = random.randint(0, len(wordList) -1)
-    # store a random index for list in variable 
+    # store a random index for list in variable
     return word_list[word_index]
-    # return the elementin list at the random index stored 
+    # return the elementin list at the random index stored
     print(wordList)
 
 def display_board(missed_letters, correct_letters, secret_word):
@@ -52,18 +52,18 @@ def display_board(missed_letters, correct_letters, secret_word):
     show correct guesses, letters guessed incorrectly and hangman progress
     '''
     print(HANGMAN_PICS[len(missed_letters)])
-    # display hangman progress based on the lenght of missed letters 
+    # display hangman progress based on the lenght of missed letters
     print()
 
     print("Missed letters:", end=' ')
-    # prints message with a space at the end rather than moving to new line 
+    # prints message with a space at the end rather than moving to new line
     for letters in missed_letters:
         # iterate through each letter in missedLetters and print
         print(letter, end=' ')
     print()
 
     blanks = '_' * len(secret_word)
-    # create blanks for secret word based on lenght of secretWord variable 
+    # create blanks for secret word based on lenght of secretWord variable
 
     for i in range(len(secret_word)):
     # iterate through secretWord, replace blanks with letters at their index if in correct letters
@@ -71,9 +71,23 @@ def display_board(missed_letters, correct_letters, secret_word):
             blanks = blanks[:i] + secret_word[i] + blanks[i+1:]
     
     for letter in blanks:
-    # show game word with spaces between each letter 
+    # show game word with spaces between each letter
         print(letter, end=' ')
     print()
 
+def get_guess(already_guessed):
+    # returns letter entered by player. Handles invalid input 
+    while True :
+        print("Guess a letter.")
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('Please enter a single letter')
+        elif guess in already_guessed:
+            print('Oops! You have already guessed that letter! Please try again.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Oops! That does not seem to be a letter, please try again using a letter.!')
+        else: 
+            return guess
 
 
